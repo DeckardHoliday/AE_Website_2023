2192027160df1996e4578e38408e5f68f5af940a
meshtatsuo <meshtatsuo@gmail.com>
2022-11-26 19:25:34 -0700

    .env example and refactored helper folder location

--- src/helpers/test_trello.js
+++ src/helpers/test_trello.js
@@ -1,5 +1,7 @@
 import * as dotenv from "dotenv"; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
-dotenv.config();
+dotenv.config({
+  path: "../../.env",
+});
 import Trello from "trello";
 const id = process.env.TRELLO_BOARD;
 const list_id = process.env.TRELLO_LIST;
