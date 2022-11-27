3b9386a26b63d1bd5d91597530f1c5da2286dabb
Dominic Misasi <meshtatsuo@gmail.com>
2022-11-27 10:45:11 -0700

    about page work

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,3 +1,43 @@
 <template>
-  <section><h4>About</h4></section>
+  <section
+    class="container mx-auto px-3 my-32 text-stone-900 dark:text-slate-100"
+  >
+    <div class="grid grid-cols-2 gap-4">
+      <div id="text p-5">
+        <h2 class="text-4xl my-8">What is Audio Elk?</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec tempor,
+          elit quis tristique sodales, felis elit interdum dolor, ut maximus
+          ipsum arcu at tortor. Phasellus fermentum augue mi, pharetra auctor
+          mauris ultrices non. Duis viverra accumsan tortor vel bibendum. Mauris
+          eu nisl luctus, malesuada ex interdum, ultricies felis. Nulla leo leo,
+          accumsan non nibh at, malesuada dictum mi. Maecenas nisi libero,
+          tristique et eleifend eu, suscipit eget enim. Ut sed augue lacinia,
+          blandit dolor sed, faucibus lectus. Nunc venenatis sollicitudin ex nec
+          tempor. Integer sollicitudin velit sed lectus facilisis fringilla.
+        </p>
+        <br />
+        <p>
+          vestibulum diam ultrices vel. Mauris ante neque, fringilla ac diam ut,
+          porta fermentum magna. Sed blandit purus turpis, faucibus feugiat
+          augue consectetur sed. Pellentesque sagittis porta odio et hendrerit.
+          Nulla facilisi. Suspendisse ut ex nulla. Etiam sed sollicitudin sem.
+          Nunc gravida ac metus non lobortis. Suspendisse auctor sapien at est
+          mattis, a tincidunt orci finibus. Praesent sodales ultricies ipsum id
+          tincidunt. Suspendisse nulla tortor, laoreet eu orci sed, fringilla
+          sodales massa. Nunc iaculis blandit justo eget dapibus. Proin
+          scelerisque volutpat lacus, ac sollicitudin felis semper ac. Vivamus
+          vel consequat sapien. Ut dictum ex orci, eget ornare neque pretium
+          vitae. Donec dapibus consectetur ligula quis viverra. Vestibulum ante
+          ipsum primis in faucibus orci luctus et ultrices posuere cubilia
+          curae; Nunc pellentesque, urna sit amet venenatis tristique, lectus
+          sem finibus velit, eget rhoncus libero erat id felis. Ut sit amet
+          luctus ante. Nunc congue eros diam, eget mattis nunc elementum in.
+        </p>
+      </div>
+      <div id="profile_image">
+        <img src="../assets/placeholder_carousel_3.png" />
+      </div>
+    </div>
+  </section>
 </template>
