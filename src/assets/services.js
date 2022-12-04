6b221e5c889397e457b8d2c6b5718e10c70f9270
meshtatsuo <meshtatsuo@gmail.com>
2022-12-03 21:55:44 -0700

    added services draft

--- src/assets/services.js
+++ src/assets/services.js
@@ -96,7 +96,77 @@ export const editing_services = [
     title: "Podcast Editing and Mixing",
     price: 100,
     per: "Hour",
-    features: [{}],
+    features: [
+      {
+        included: true,
+        description: "Filler word cleanup (ums, ahs)",
+      },
+      {
+        included: true,
+        description: "Silence removal",
+      },
+      {
+        included: true,
+        description: "Pace and structural editing",
+      },
+      {
+        included: true,
+        description: "Ad Placements and loudness matching",
+      },
+      {
+        included: true,
+        description: "Mixing and mastering for publication",
+      },
+    ],
   },
   {},
+];
+
+export const music_services = [
+  {
+    title: "Music Mixing",
+    price: 150,
+    per: "Song",
+    features: [
+      {
+        included: true,
+        description: "Full mix of one song",
+      },
+      {
+        included: true,
+        description: "5 revisions",
+      },
+      {
+        included: true,
+        description: "Master ready mix with plenty of headroom",
+      },
+      {
+        included: false,
+        description: "Mastering services excluded",
+      },
+    ],
+  },
+  {
+    title: "Music Mastering",
+    price: 150,
+    per: "Song",
+    features: [
+      {
+        included: true,
+        description: "Full master of one song",
+      },
+      {
+        included: true,
+        description: "5 revisions",
+      },
+      {
+        included: true,
+        description: "Ready to publish finals for your platform of choice",
+      },
+      {
+        included: false,
+        description: "Additional platform masters not included",
+      },
+    ],
+  },
 ];
