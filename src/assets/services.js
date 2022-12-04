6e755a13d35483c5ef8e04569c806ca2cd56d221
Dominic Misasi <meshtatsuo@gmail.com>
2022-12-03 20:15:46 -0700

    added services

--- src/assets/services.js
+++ src/assets/services.js
@@ -21,9 +21,82 @@ export const post_services = [
         included: true,
         description: "24 bit/48 kHz WAV export",
       },
+      {
+        included: false,
+        description: "High complex scenes not included",
+      },
+      {
+        included: false,
+        description: "3 Character Limit",
+      },
     ],
   },
   {
-    //
+    title: Premium,
+    price: 50,
+    per: "15 Seconds",
+    features: [
+      {
+        included: true,
+        description: "5 Revisions",
+      },
+      {
+        included: true,
+        description: "Custom foley and design work",
+      },
+      {
+        included: true,
+        description: "1080p, 720p MP4 Exports",
+      },
+      {
+        included: true,
+        description: "24 bit/48 kHz WAV export",
+      },
+      {
+        included: true,
+        description: "Includes complex scenes",
+      },
+      {
+        included: true,
+        description: "No character limit",
+      },
+    ],
   },
+];
+
+export const editing_services = [
+  {
+    title: "Cleanup and Restoration",
+    price: 50,
+    per: "15 Minutes",
+    features: [
+      {
+        included: true,
+        description: "Spectral repair and noise removal",
+      },
+      {
+        included: true,
+        description: "Filler word cleanup (ums, ahs)",
+      },
+      {
+        included: true,
+        description: "Silence removal",
+      },
+      {
+        included: false,
+        description: "Pace editing",
+      },
+      {
+        included: false,
+        description: "Mixing and mastering",
+      },
+    ],
+  },
+  {
+    title: "Podcast Editing and Mixing",
+    price: 100,
+    per: "Hour",
+    features: [{}],
+  },
+  {},
 ];
