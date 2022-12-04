f604e80b766a0f1fa4dde44f2e83bf124b18f222
meshtatsuo <meshtatsuo@gmail.com>
2022-12-03 18:18:00 -0700

    added price card component

---
+++ src/assets/services.js
@@ -0,0 +1,29 @@
+export const post_services = [
+  {
+    // Standard Tier
+    title: Standard,
+    price: 35,
+    per: "15 Seconds",
+    features: [
+      {
+        included: true,
+        description: "3 Revisions",
+      },
+      {
+        included: true,
+        description: "Custom foley and design work",
+      },
+      {
+        included: true,
+        description: "1080p, 720p MP4 Exports",
+      },
+      {
+        included: true,
+        description: "24 bit/48 kHz WAV export",
+      },
+    ],
+  },
+  {
+    //
+  },
+];
