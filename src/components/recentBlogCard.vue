46fbbf8ab5ddb52b86dd5a20c96d574a1aa913a5
meshtatsuo <meshtatsuo@gmail.com>
2023-01-28 10:12:45 -0700

    blog post fixed, recent blogs implemented

---
+++ src/components/recentBlogCard.vue
@@ -0,0 +1,34 @@
+<script>
+export default {
+  props: {
+    title: String,
+    date_posted: Date,
+    content: String,
+    url: String,
+  },
+};
+</script>
+
+<template>
+  <div
+    class="my-5 p-3 w-80 rounded dark:bg-black dark:border-white dark:border"
+  >
+    <a :href="url" target="_blank">
+      <h2
+        class="text-left md:text-left lg:text-left mb-1 text-lg font-extrabold leading-none text-gray-900 md:text-4xl lg:text-xl dark:text-white"
+      >
+        {{ title }}
+      </h2>
+      <h5
+        class="text-right md:text-right lg:text-left text-xs mb-1 font-normal leading-none text-gray-900 md:text-4xl lg:text-lg dark:text-white"
+      >
+        {{ new Date(date_posted).toDateString() }}
+      </h5>
+      <hr class="mb-3" />
+      <p class="mb-5">{{ content }}</p>
+      <a :href="url" target="_blank" class="text-lg text-right"
+        ><p>Read More...</p></a
+      >
+    </a>
+  </div>
+</template>
--- src/pages/blog.vue
+++ src/pages/blog.vue
@@ -1,8 +1,7 @@
 <script>
 import PageTitle from "../components/pagetitle.vue";
+import RecentBlogCard from "../components/recentBlogCard.vue";
 import SectionTitle from "../components/sectiontitle.vue";
-
-console.log(import.meta.env.VITE_BLOGGER_API_KEY);
 
 const res = await fetch(
   `https://www.googleapis.com/blogger/v3/blogs/4667748673448071263/posts?key=${
@@ -14,17 +13,37 @@ const blog_data = await res.json();
 
 const blog_items = blog_data.items;
 
-console.log(blog_items);
+const latest_blog = blog_items[0];
+const recent_posts = blog_items;
+
+console.log(latest_blog);
+console.log(recent_posts);
 
 export default {
   components: {
     PageTitle,
     SectionTitle,
+    RecentBlogCard,
   },
   data() {
     return {
-      blog_items,
+      latest_blog,
+      recent_posts,
     };
+  },
+  methods: {
+    create_excerpt(string) {
+      let max_length = 240;
+
+      let excerpt = string.replace(/(<([^>]+)>)/gi, "");
+      excerpt = excerpt.replace("&nbsp;", "");
+
+      excerpt = excerpt.substring(0, max_length);
+
+      excerpt += "...";
+
+      return excerpt;
+    },
   },
 };
 </script>
@@ -32,19 +51,24 @@ export default {
 <template>
   <PageTitle title="Blog" />
   <div class="flex min-h-screen">
-    <div class="flex-1 max-w-5xl text-white ml-64 mr-16">
+    <div class="flex-1 max-w-4xl text-white ml-64 mr-32 p-5">
       <h2
         class="text-left md:text-left lg:text-left mb-2 text-4xl font-extrabold leading-none text-gray-900 md:text-4xl lg:text-4xl dark:text-white"
       >
-        {{ blog_items[0].title }}
+        {{ latest_blog.title }}
       </h2>
+      <h5
+        class="text-right md:text-right lg:text-left mb-3 text-xs font-normal leading-none text-gray-900 md:text-4xl lg:text-lg dark:text-white"
+      >
+        {{ new Date(latest_blog.published).toDateString() }}
+      </h5>
+      <hr class="mb-5" />
+      <div v-html="latest_blog.content"></div>
       <h5
         class="text-right md:text-right lg:text-left mb-8 text-xs font-normal leading-none text-gray-900 md:text-4xl lg:text-lg dark:text-white"
       >
-        {{ new Date(blog_items[0].published).toDateString() }}
+        <a :href="latest_blog.url" target="_blank">View Original Post</a>
       </h5>
-      <hr />
-      {{ blog_items[0].content }}
     </div>
     <div class="flex-0 text-white">
       <h2
@@ -53,6 +77,15 @@ export default {
         Recent Posts
       </h2>
       <hr />
+      <ul v-for="item in recent_posts">
+        <recent-blog-card
+          :key="item.title.split(' ').join('_')"
+          :title="item.title"
+          :content="create_excerpt(item.content)"
+          :date_posted="item.published"
+          :url="item.url"
+        />
+      </ul>
     </div>
   </div>
 </template>
